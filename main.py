a564b68f01c0668c840ebd0443bea6d6cdc9aba3
Kalu Gift <84230572+giftkalu@users.noreply.github.com>
2025-09-22 21:50:05 +0100

    Update main.py BACK TO OG!

--- main.py
+++ main.py
@@ -198,7 +198,4 @@ async def voice_assist(audio: UploadFile = File(...)):
         
 if __name__ == "__main__":
     import uvicorn
-    import os
-    
-    port = int(os.environ.get("PORT", 8000))  # use Render's PORT if available
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
